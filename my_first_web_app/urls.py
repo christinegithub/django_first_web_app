66e747332a9c23aa139cd584f8b22fff4275cb87
christinegithub <christine13@gmail.com>
2019-02-28 15:10:33 -0500

    views.py added

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -13,43 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from random import randint
-from django.http import HttpResponse
-from django.shortcuts import render
+
 from django.urls import path
-from django.template import Template, Context
+from my_first_web_app.views import home_page, portfolio_page, about_me_page, favourites_page
 
-def home_page(request):
-    context = {'name': 'Betty Maker'}
-    response = render(request, 'index.html', context)
-    return HttpResponse(response)
-
-def portfolio_page(request):
-
-    image_urls = []
-    for i in range(5):
-        random_number = randint(0,100)
-        image_urls.append("https://picsum.photos/400/600/?image={}".format(random_number))
-
-
-    context = {'gallery_images': image_urls}
-    response = render(request, 'gallery.html', context)
-    return HttpResponse(response)
-
-def about_me_page(request):
-    skills = ['html', 'css', 'python']
-    interests = ['dance', 'travel']
-
-    context = {'skills': skills, 'interests': interests}
-    response = render(request, 'about_me.html', context)
-    return HttpResponse(response)
-
-def favourites_page(request):
-
-    fave_links = ["https://www.google.com/", "https://www.facebook.com/"]
-    context = {'links': fave_links}
-    response = render(request, 'favourites.html', context)
-    return HttpResponse(response)
 
 
 urlpatterns = [
---
+++ my_first_web_app/views.py
@@ -0,0 +1,35 @@
+from random import randint
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    context = {'name': 'Betty Maker'}
+    response = render(request, 'index.html', context)
+    return HttpResponse(response)
+
+def portfolio_page(request):
+
+    image_urls = []
+    for i in range(5):
+        random_number = randint(0,100)
+        image_urls.append("https://picsum.photos/400/600/?image={}".format(random_number))
+
+
+    context = {'gallery_images': image_urls}
+    response = render(request, 'gallery.html', context)
+    return HttpResponse(response)
+
+def about_me_page(request):
+    skills = ['html', 'css', 'python']
+    interests = ['dance', 'travel']
+
+    context = {'skills': skills, 'interests': interests}
+    response = render(request, 'about_me.html', context)
+    return HttpResponse(response)
+
+def favourites_page(request):
+
+    fave_links = ["https://www.google.com/", "https://www.facebook.com/"]
+    context = {'links': fave_links}
+    response = render(request, 'favourites.html', context)
+    return HttpResponse(response)
